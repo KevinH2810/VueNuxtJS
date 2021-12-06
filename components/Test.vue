5b50ff78426e6d75b27426a734c02d87a55207da
Kevin Hadi <47102992+KevinH2810@users.noreply.github.com>
2021-12-06 20:13:06 +0800

    starter

---
+++ components/Test.vue
@@ -0,0 +1,27 @@
+.<template>
+  <h1>{{title}}</h1>
+</template>
+
+<script>
+  module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+</script>
+
+<style>
+
+</style>
